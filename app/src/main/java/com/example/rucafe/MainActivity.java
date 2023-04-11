3466367fb8bd7ee9930f296b63c3793547516ebe
JasonGuo00 <jasonglobguo@gmail.com>
2023-04-11 16:53:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.rucafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button donut, coffee, basket, list;
+    // Fragment Manager
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Buttons
+        donut = findViewById(R.id.donut);
+        coffee = findViewById(R.id.coffee);
+        basket = findViewById(R.id.basket);
+        list = findViewById(R.id.list);
+
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    public void transition_donut(View view) {
+        findViewById(R.id.home).setVisibility(View.INVISIBLE);
+        DonutFragment donut_frag = (DonutFragment)fragmentManager.findFragmentByTag("DonutFragment");
+        if(donut_frag == null) {
+            donut_frag = new DonutFragment();
+            FragmentTransaction fragmentTransaction  = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_view, donut_frag, "DonutFragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void transition_coffee(View view) {
+        findViewById(R.id.home).setVisibility(View.INVISIBLE);
+        CoffeeFragment coffee_frag = (CoffeeFragment) fragmentManager.findFragmentByTag("CoffeeFragment");
+        if(coffee_frag == null) {
+            coffee_frag = new CoffeeFragment();
+            FragmentTransaction fragmentTransaction  = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_view, coffee_frag, "CoffeeFragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void transition_basket(View view) {
+        findViewById(R.id.home).setVisibility(View.INVISIBLE);
+        OrderFragment order_frag = (OrderFragment) fragmentManager.findFragmentByTag("OrderFragment");
+        if(order_frag == null) {
+            order_frag = new OrderFragment();
+            FragmentTransaction fragmentTransaction  = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_view, order_frag, "OrderFragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void transition_shoplist(View view) {
+        findViewById(R.id.home).setVisibility(View.INVISIBLE);
+        ShoplistFragment shoplist_frag = (ShoplistFragment) fragmentManager.findFragmentByTag("ShoplistFragment");
+        if(shoplist_frag == null) {
+            shoplist_frag = new ShoplistFragment();
+            FragmentTransaction fragmentTransaction  = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.main_view, shoplist_frag, "ShoplistFrag");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        findViewById(R.id.home).setVisibility(View.VISIBLE);
+        super.onBackPressed();
+    }
+
+}
