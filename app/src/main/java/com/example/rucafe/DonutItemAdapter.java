8777221c50c406d28ba646234e38bc2df3c4faa5
JasonGuo00 <jasonglobguo@gmail.com>
2023-04-12 11:59:30 -0400

    Donut View Done (Pretty much)
    Last thing that needs to be added:
    
    - Add to order button
    
    Waiting until we have that more fleshed out before adding it.

--- app/src/main/java/com/example/rucafe/DonutItemAdapter.java
+++ app/src/main/java/com/example/rucafe/DonutItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.rucafe;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,20 +10,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import cafeapp.RecyclerClickListener;
 import cafeapp.RecyclerDonut;
 
 
 public class DonutItemAdapter extends RecyclerView.Adapter<DonutItem>{
     private ArrayList<RecyclerDonut> donut_options;
+    private Context context;
 
-    public DonutItemAdapter(ArrayList<RecyclerDonut> list) {
+    private RecyclerClickListener listener;
+
+    public DonutItemAdapter(Context context, ArrayList<RecyclerDonut> list, RecyclerClickListener listener) {
+        this.context = context;
         donut_options = list;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public DonutItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(R.layout.donut_item_layout, parent, false);
         return new DonutItem(view);
     }
@@ -30,10 +37,12 @@ public class DonutItemAdapter extends RecyclerView.Adapter<DonutItem>{
     @Override
     public void onBindViewHolder(@NonNull DonutItem holder, int position) {
         holder.donut_desc.setText(donut_options.get(position).toString());
+        holder.donut_img.setImageResource(donut_options.get(position).getImg_id());
+        holder.set_listener(position, listener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return donut_options.size();
     }
 }
